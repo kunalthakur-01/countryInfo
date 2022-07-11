7eafa616aa093bd220eec409cca6e93505cf56c4
kunal <thakurrajput98765@gmail.com>
2022-07-11 11:58:48 +0530

    added neighbour country

--- script.js
+++ script.js
@@ -3,22 +3,9 @@
 const container = document.querySelector('.container');
 
 
-const getCountry = function (country,i) {
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-
-
-    request.send();
-    // console.log(request.responseText)
-
-    request.addEventListener('load', function () {
-        // console.log(this.responseText);
-
-        const [data] = JSON.parse(this.responseText);
-        // console.log(data);
-
-        const html =
-        ` <div class="countries">
+const renderCountry = function (data, i, className = '') {
+    const html =
+        ` <div class="countries ${className}">
             <div class="country-flag country-flag-${i}">
             </div>
             <div class="country-info">
@@ -27,7 +14,7 @@ const getCountry = function (country,i) {
                 <ul>
                     <li class="population">🧑‍🤝‍🧑&nbsp; &nbsp;<span>${(data.population / 1000000).toFixed(2)}M people</span></li>
                     <li class="language">🗣️ &nbsp;&nbsp;<span>${data.languages[Object.keys(data.languages)[0]]}</span></li>
-                    <li class="currrency">💵 &nbsp;&nbsp;<span>${data.currencies[Object.keys(data.currencies)[0]].name}(${data.currencies[Object.keys(data.currencies)[0]].symbol})</span></li>
+                    <li class="currrency">💰 &nbsp;&nbsp;<span>${data.currencies[Object.keys(data.currencies)[0]].name}(${data.currencies[Object.keys(data.currencies)[0]].symbol})</span></li>
                 </ul>
             </div>
         </div>
@@ -36,12 +23,41 @@ const getCountry = function (country,i) {
     container.insertAdjacentHTML('beforeend', html);
     const flag = document.querySelector(`.country-flag-${i}`);
     flag.style.background = `url(${data.flags.png})`;
-    });
+    flag.style.backgroundPosition = 'center';
+    flag.style.backgroundSize = 'cover';
+    flag.style.backgroundRepeat = 'no-repeat';
 }
 
-const country1 = prompt('Enter first country name');
-const country2 = prompt('Enter second country name');
 
-getCountry(country1,'1');
+const getCountryNeighbour = function (country,i) {
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+    request.send();
 
-getCountry(country2,'2');
+    request.addEventListener('load', function () {
+        const [data] = JSON.parse(this.responseText);
+        console.log(data)
+
+        // render the country
+        renderCountry(data, '1');
+
+        // render the neibhgour country
+        const [neighbour] = data.borders;
+
+        if(!neighbour) return;
+
+        const request = new XMLHttpRequest();
+        request.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+        request.send();
+
+        request.addEventListener('load', function () {
+            const data2 = JSON.parse(this.responseText);
+            console.log(data2)
+
+            renderCountry(data2[0], '2', 'neighbour');
+        })
+    });
+        
+}
+
+getCountryNeighbour('mexico');
